b8572a20bf930d3e1f97ec0c2ad384218a2a20a0
victor <Victor Carlquist>
2024-04-26 16:41:14 -0300

    Changes some colors

--- src/components/weatherDetail.tsx
+++ src/components/weatherDetail.tsx
@@ -4,7 +4,7 @@ import { Stars } from "react-bootstrap-icons";
 function WeatherDetail(props: { weatherDetail: any; }) {
 
   return (
-    <Card className="card-glass-dark" body>
+    <Card className="card-glass-dark card-glass-desc" body>
         <Stars></Stars>
       {props.weatherDetail}
     </Card>
